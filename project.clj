4f62ac977dbbe4d4d7e3427e900dd74a064795d2
Karthikeyan Chinnakonda <kartthikspiky@gmail.com>
2018-08-01 13:29:36 +0530

    Update the project.clj with correct github repo link

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure Library for the Sendgrid API v3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "http://slaejae.com"
+  :url "https://github.com/codingkarthik/clj-sendgrid"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.1"]
                  [environ "1.1.0"]
@@ -15,10 +15,4 @@
   :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
                                      :creds :gpg}
                          "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]]
-  :scm {:url "git@github.com:anthontaylor/clj-sendgrid.git"}
-  :pom-addition [:developers [:developer
-                              [:name "AJ Taylor"]
-                              [:url "http://slaejae.com"]
-                              [:email "info@slaejae.com"]
-                              [:timezone "-5"]]])
+                                      :creds :gpg}]])
